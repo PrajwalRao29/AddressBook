28497abc3573e840573d260b4f53c6e08d21f3a4
PrajwalRao29 <raoprajrao@gmail.com>
2020-09-28 21:40:33 +0530

    Added edit and view functionalities to the program

--- Address.java
+++ Address.java
@@ -18,10 +18,34 @@ class Contact {
 		System.out.println("Enter zip code");
 		this.zip=sc.next();
 		System.out.println("Enter phone number");
-		String phno=sc.next();
+		this.phno=sc.next();
 		System.out.println("Enter email");
-		String email=sc.next();
+		this.email=sc.next();
 	}
+	public void editContact()
+	{
+		System.out.println("Enter Address");
+		this.address=sc.next();
+		System.out.println("Enter city");
+		this.city=sc.next();
+		System.out.println("Enter state");
+		this.state=sc.next();
+		System.out.println("Enter zip code");
+		this.zip=sc.next();
+		System.out.println("Enter phone number");
+		this.phno=sc.next();
+		System.out.println("Enter email");
+		this.email=sc.next();	
+	}
+	public void viewContact()
+	{
+		System.out.println("ADDRESS = "+this.address);
+		System.out.println("CITY = "+this.city);	
+		System.out.println("STATE = "+this.state);	
+		System.out.println("ZIP = "+this.zip);	
+		System.out.println("PHONE NUMBER ="+this.phno);	
+		System.out.println("EMAIL ="+this.email);	
+	}	
 }
 public class Address {
 	public static void main(String args[])
@@ -30,16 +54,66 @@ public class Address {
 		System.out.println("Welcome to Address Book program");
 		Scanner sc=new Scanner(System.in);
 		int x=0;
-		while(x!=2)
+		while(x!=4)
 		{
 			System.out.println("1.ADD A CONTACT");
-			System.out.println("2.EXIT");
+			System.out.println("2.EDIT A CONTACT BASED ON NAME");
+			System.out.println("3.VIEW CONTACT DETAILS BASED ON NAME");
+			System.out.println("3.EXIT");
 			x=sc.nextInt();
 			if(x==1)
 			{
 				Contact a=new Contact();
 				a.addContact();
 				c.add(a);
+			}
+			if(x==2)
+			{
+				System.out.println("Enter First Name");
+				String f=sc.next();
+				System.out.println("Enter Last Name");
+				String l=sc.next();	
+				int k=0,i=0;
+				for(i=0;i<c.size();i++)
+				{
+					if(c.get(i).first.equalsIgnoreCase(f) && c.get(i).last.equalsIgnoreCase(l))
+					{
+						k=1;
+						break;
+					}
+				}
+				if(k==0)
+				{
+					System.out.println("******No Contact found******");
+				}
+				else
+				{
+					c.get(i).editContact();
+				}
+			}
+			if(x==3)
+			{
+				System.out.println("Enter First Name");
+				String f=sc.next();
+				System.out.println("Enter Last Name");
+				String l=sc.next();	
+				int k=0,i=0;
+				for(i=0;i<c.size();i++)
+				{
+					if(c.get(i).first.equalsIgnoreCase(f) && c.get(i).last.equalsIgnoreCase(l))
+					{
+						k=1;
+						break;
+					}
+				}
+				if(k==0)
+				{
+					System.out.println("******No Contact found******");
+				}
+				else
+				{
+					c.get(i).viewContact();
+				}		
 			}
 		}
 	}
