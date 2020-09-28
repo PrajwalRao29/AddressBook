a7102f967ee008e4bba5d039eae4f7092a153c93
PrajwalRao29 <raoprajrao@gmail.com>
2020-09-28 21:12:20 +0530

    Added functionality to create contacts

--- Address.java
+++ Address.java
@@ -1,4 +1,24 @@
-
+import java.util.*;
+class Contact {
+	String first,last,address,String,city,state,zip,phno,email;
+	ArrayList <Contact> c=new ArrayList<Contact>();
+	public Contact(String first,String last,String address,String city,String state,String zip,String phno,String email)
+	{
+		this.first=first;
+		this.last=last;
+		this.address=address;
+		this.city=city;
+		this.state=state;
+		this.zip=zip;
+		this.phno=phno;
+		this.email=email;
+	}
+}
 public class Address {
-
+public static void main(String args[])
+{
+	System.out.println("Welcome to Address Book program");
+	Scanner sc=new Scanner(System.in);
+	
+}
 }
